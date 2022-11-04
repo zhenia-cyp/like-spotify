8d2a66aab4a2f9b4609a39e63395ce61b0112e74
zhenia kviatkivskiy <zhenia.kviat@gmail.com>
2022-11-04 18:23:52 +0200

    refactor: made changes in sattings.py

--- likespotify/config/settings.py
+++ likespotify/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vuw9qqtr=mmmxq0i00psxr9f$&3%+0unfwci8442b52+pqmmxf'
+SECRET_KEY = 'django-insecure-9qqtr=mmmxq0i00psxr9f$&3%+0unfwci8442b52+pqmmx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
