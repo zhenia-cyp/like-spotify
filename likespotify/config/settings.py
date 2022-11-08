d6f1016b4c2881625d8baece3c25ff594362efd4
zhenia kviatkivskiy <zhenia.kviat@gmail.com>
2022-11-08 14:58:24 +0200

    refactor: update requirements.txt

--- likespotify/config/settings.py
+++ likespotify/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9qqtr=mmmxq0i00psxr9f$&3%+0unfwci8442b52+pqmmx'
+SECRET_KEY = 'django-insecure-vuw9qqtr=mmmxq0i00psxr9f$&3%+0unfwci8442b52+pqmmxf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
